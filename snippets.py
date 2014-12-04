04c1e27e31b240f85c8606de2b4630f52136406c
sitthipol rujirawat <rsitthipol@hotmail.com>
2014-12-03 19:01:38 -0800

    second commit

--- snippets.py
+++ snippets.py
@@ -1,12 +1,13 @@
 import logging
 import csv
-
+import argparse
+import sys
 #Set the log output file, and the log level
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
 
-def put(name, snippet, filename)
-""" Store a snippet with an associated name in the CSV file """
-	logging.info("Writing {!r}:{!r} to {!r}").format(name,snippet,filename)
+def put(name, snippet, filename):
+	""" Store a snippet with an associated name in the CSV file """
+	logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
 	logging.debug("Openning file")
 	with open(filename, "a") as f:
 		writer = csv.writer(f)
@@ -15,4 +16,33 @@ def put(name, snippet, filename)
 	logging.debug("Write successful")
 	return name, snippet
 
-	
+def make_parser():
+	""" Construct the command line parser """
+	logging.info("Constructing parser")
+	description = "Store and retrieve snippets of text"
+	parser = argparse.ArgumentParser(description=description)
+	subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+	# Subparser for the put command
+	logging.debug("Constructing put subparser")
+	put_parser = subparsers.add_parser("put", help="Store a snippet")
+	put_parser.add_argument("name", help="The name of the snippet")
+	put_parser.add_argument("snippet", help="The snippet text")
+	put_parser.add_argument("filename", default="snippets.csv", nargs="?", help="The snippet filename")
+	return parser
+
+def main():
+	""" Main function """
+	logging.info("starting snippets")
+	parser = make_parser()
+	arguments = parser.parse_args(sys.argv[1:])
+	# Convert parsed arguments from Namespace to dictionary
+	arguments = vars(arguments)
+	command = arguments.pop("command")
+
+	if command == "put":
+		name, snippet = put(**arguments)
+		print "Stored {!r} as {!r}".format(snippet, name)
+
+if __name__ == "__main__":
+	main()
