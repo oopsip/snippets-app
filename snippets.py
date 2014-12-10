8b6fff7ab4d9defba0b904997fe66b9d49e58c7c
sitthipol rujirawat <rsitthipol@hotmail.com>
2014-12-10 12:01:32 -0800

    updated

--- snippets.py
+++ snippets.py
@@ -31,7 +31,26 @@ def get(name, filename):
 				print "there is no match"
 
 				#logging.debug("reading succcessful")
-	
+def search(searchname, filename):
+	logging.debug("Openning file")
+	with open(filename) as f:
+		reader = csv.DictReader(f, delimiter=',')
+		logging.debug("reading snippet from file")
+		for line in reader:
+			#string = line['csvname']
+			if line['csvname'].startswith(searchname) == True:
+				print line['csvname']
+
+def updatesnippet(name, snippet, filename):
+	logging.debug("Openning file")
+	with open(filename) as f:
+		reader = csv.DictReader(f, delimiter=',')
+		logging.debug("reading snippet from file")
+		for line in reader:
+			if name == line['csvname']:
+				line['csvname'] = snippet
+				print "updated succesfully"
+
 def make_parser():
 	""" Construct the command line parser """
 	logging.info("Constructing parser")
@@ -49,6 +68,15 @@ def make_parser():
 	get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
 	get_parser.add_argument("name", help="The name of the snippet")
 	get_parser.add_argument("filename", default="snippets.csv", nargs="?", help="The snippet filename")
+    #Subparser for the search command
+	search_parser = subparsers.add_parser("search", help="search for a snippet")
+	search_parser.add_argument("searchname", help="The name of the snippet")
+	search_parser.add_argument("filename", default="snippets.csv", nargs="?", help="The snippet filename")
+	#Ubdate the snippet name
+	update_parser = subparsers.add_parser("update", help="Update a snippet")
+	update_parser.add_argument("name", help="The name of the snippet")
+	update_parser.add_argument("snippet", help="The snippet text")
+	update_parser.add_argument("filename", default="snippets.csv", nargs="?", help="The snippet filename")
 
 	return parser
 
@@ -67,8 +95,14 @@ def main():
 		print "Stored {!r} as {!r}".format(snippet, name)
 	elif command == "get":
 		name = get(**arguments)
-	else:
-		print "please put command"
+	elif command == "search":
+	    searchname = search(**arguments)
+	elif command == "update":
+		name, snippet = updatesnippet(**arguments)
+		print "update {!r} with {!r}".format(name, snippet)
+	else: 
+		print "you are bad"
+	
 
 if __name__ == "__main__":
 	main()
